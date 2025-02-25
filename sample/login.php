6f1cb29108df18c243029cca2f56fb54d62f9cf7
oae26 <oae26@njit.edu>
2025-02-25 13:54:37 -0500

    adjusted login.php to send requests to RabbitMQ!

--- sample/login.php
+++ sample/login.php
@@ -1,5 +1,7 @@
 <?php
-
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
 
 if (!isset($_POST))
 {
@@ -8,14 +10,42 @@ if (!isset($_POST))
 	exit(0);
 }
 $request = $_POST;
+
+$username = $_POST['username'] ?? ' ';
+$password = $_POST['password'] ?? ' ';
+try{
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+echo "hi! we are sending things, yay.";
+} catch(Exception $e){
+	error_log("RabbitMQClient error, could not connect" . $e ->getMessage());
+	die("error, please see log for deets, bye");
+
+}
+
 $response = "unsupported request type, politely FUCK OFF";
+
 switch ($request["type"])
 {
 	case "login":
-		$response = "successfully logged in, yeah we CAN PROBABLY do that";
+	echo "login success!!";
+	$RMQrequest = array();
+	$RMQrequest['type'] = 'login';
+	$RMQrequest['username'] = $username;
+	$RMQrequest['password'] = $password;
+	$RMQresponse = $client -> send_request($RMQrequest);
+	$response = 'logged in!';
+	echo "yahooo";
+	if($RMQresponse['status'] == "success"){
+	echo "we did it !!";
+	die("hi");
+	}
+	else{
+	echo "we messed up :(";
+	}
 	break;
 }
-echo json_encode($response);
+echo json_encode($RMQresponse);
+echo("hello");
 exit(0);
 
 ?>
