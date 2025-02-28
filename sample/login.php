462f3f189d0acaaf60bef9477d1c65ac1d47be4a
oae26 <oae26@njit.edu>
2025-02-28 11:09:25 -0500

    authentication progress, RMQ now sends to client just fine.

--- sample/login.php
+++ sample/login.php
@@ -15,7 +15,6 @@ $username = $_POST['username'] ?? ' ';
 $password = $_POST['password'] ?? ' ';
 try{
 $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
-echo "hi! we are sending things, yay.";
 } catch(Exception $e){
 	error_log("RabbitMQClient error, could not connect" . $e ->getMessage());
 	die("error, please see log for deets, bye");
@@ -27,25 +26,16 @@ $response = "unsupported request type, politely FUCK OFF";
 switch ($request["type"])
 {
 	case "login":
-	echo "login success!!";
 	$RMQrequest = array();
 	$RMQrequest['type'] = 'login';
 	$RMQrequest['username'] = $username;
 	$RMQrequest['password'] = $password;
 	$RMQresponse = $client -> send_request($RMQrequest);
-	$response = 'logged in!';
-	echo "yahooo";
-	if($RMQresponse['status'] == "success"){
-	echo "we did it !!";
-	die("hi");
 	}
-	else{
-	echo "we messed up :(";
-	}
-	break;
-}
-echo json_encode($RMQresponse);
-echo("hello");
-exit(0);
+	
+	$json = json_encode($RMQresponse);
+	var_dump($json);
+	return($json);
+	exit(0);
 
 ?>
