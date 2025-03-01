c1d08c1ed00d70f61fc7f9585fb185f7fee12c87
Franklin <fgw@njit.edu>
2025-03-01 15:59:32 -0500

    authentication done

--- mysqlRabbitConnect.php
+++ mysqlRabbitConnect.php
@@ -43,12 +43,18 @@ function doLogin($username,$password)
         
         
         if($username == $db_username && $password == $db_password){
-        	echo "Sending back valid login" . PHP_EOL;
-        	return array('requestProcessor',array("returnCode" => '0', 'message'=>"Matching username and password found"));
+        	$sessionKey = rand(0, 999);
+        	echo $sessionKey . PHP_EOL;
+        	echo "Inserting session key into sessions table where username = fetched db username". PHP_EOL;
+        	$sessionInsertQuery = "update sessions set sessionKey = " . $sessionKey . " where username = '" . $db_username . "';";
+        	if($sessionResponse = $mydb->query($sessionInsertQuery)){
+        		echo "Sending back valid login/username and session key" . PHP_EOL;
+        		return array('returnCode' => '0', 'message'=>$db_username, 'sessionKey'=>$sessionKey);
+        	}
         } 
         else{
         	echo "invalid credentials" . PHP_EOL;
-        	return array('requestProcessor',array("returnCode" => '1', 'message'=>"Invalid credentials"));
+        	return array('returnCode' => '1', 'message'=>'Invalid credentials');
         }
         
 }
@@ -63,23 +69,97 @@ function doRegister($username,$password)
        
         if ($response = $mydb->query($query)){
         	if($response->num_rows == 0){
+        		echo "Username not in use, registering..." . PHP_EOL;
         		$insertQuery = "insert into users (username, password) values ('" . $username ."', '" . $password . "');";
         		if($insertResponse = $mydb->query($insertQuery)){
-        			return array('requestProcessor',array("returnCode" => '0', 'message'=>"User registered"));
+        			$sessionInsertQuery = "insert into sessions (username) values ('" . $username ."')";
+        			if($sessionInsertResponse = $mydb->query($sessionInsertQuery)){
+        				echo "User registered, sessions table entry created" . PHP_EOL;
+        				return array("returnCode" => '0', 'message'=>"User registered");
+        			}
+        			else if ($mydb->errno != 0)
+				{
+					return array('returnCode' => '2', 'message'=>'Query invalid');
+				}
+        			
         		}
+        		else if ($mydb->errno != 0)
+			{
+				return array('returnCode' => '2', 'message'=>'Query invalid');
+			}
+        	}
+        	//if query is borked
+        	else{
+        		echo "Username found, cancelling registration" . PHP_EOL;
+        		return array('returnCode' => '1', 'message'=>'Username already in use');
         	}
         }
-        //if query is borked
-        else if ($mydb->errno != 0)
-	{
-		return array('requestProcessor',array("returnCode" => '2', 'message'=>"Query invalid"));
-	}
-        //if query returned a matching username
-        else{
-        	return array('requestProcessor',array("returnCode" => '1', 'message'=>"Username already in use"));
-        }
-        	
         
+        if ($mydb->errno != 0)
+	{
+		return array('returnCode' => '2', 'message'=>'Query invalid');
+	}
+}
+
+function doValidate($username, $sessionKey){
+	global $db_username;
+	global $db_sessionKey;
+	global $mydb;
+	
+	$query = "select * from sessions where username = '" . $username . "';";
+	
+	if ($response = $mydb->query($query)){
+        	while($row = $response -> fetch_row()){
+        		$db_username = $row[0];
+        		$db_sessionKey = $row[1];
+        	}	
+        	if($username == $db_username){
+        		if($db_sessionKey === null){
+        			echo "Session key is null, invalid" . PHP_EOL;
+        			return array('returnCode' => '1', 'message'=>'Session invalid');
+        		}
+        		else if ($sessionKey == $db_sessionKey){
+        			echo "Key's match, session valid" . PHP_EOL;
+        			return array('returnCode' => '0', 'message'=>'Session validated');
+        		}
+        		else if ($sessionKey != $db_sessionKey){
+        			echo "Key's mismatch, session invalid" . PHP_EOL;
+        			return array('returnCode' => '1', 'message'=>'Session invalid');
+        		}
+        	}
+	}
+	if ($mydb->errno != 0)
+	{
+		return array('returnCode' => '2', 'message'=>'Query invalid');
+	}
+}
+
+function doLogout($username, $sessionKey){
+	global $db_username;
+	global $db_sessionKey;
+	global $mydb;
+	
+	$query = "select * from sessions where username = '" . $username . "' and sessionKey = " . $sessionKey . ";";
+	
+	if ($response = $mydb->query($query)){
+        	while($row = $response -> fetch_row()){
+        		$db_username = $row[0];
+        		$db_sessionKey = $row[1];
+        	}	
+        	if($username == $db_username && $sessionKey == $db_sessionKey){
+        		$logoutQuery = "update sessions set sessionKey = NULL where username = '" . $db_username . "';";
+        		if($logoutResponse = $mydb->query($logoutQuery)){
+        			if ($mydb->errno == 0) {
+					echo "user session key nulled, logout okay" . PHP_EOL;
+					return array('returnCode' => '0', 'message'=>'Logout Successful');
+				}
+        		}
+        	}
+	}
+	if ($mydb->errno != 0)
+	{
+		return array('returnCode' => '2', 'message'=>'Query invalid');
+	}
 }
 
 
@@ -89,20 +169,22 @@ function requestProcessor($request)
   var_dump($request);
   if(!isset($request['type']))
   {
-    return "ERROR: unsupported message type";
+  	return "ERROR: unsupported message type";
   }
   switch ($request['type'])
   {
     case "login":
-      return doLogin($request['username'],$request['password']);
+    	return doLogin($request['username'],$request['password']);
     case "register":
     	return doRegister($request['username'],$request['password']);
     case "validate_session":
-      return doValidate($request['username'],$request['sessionId']);
+    	return doValidate($request['username'],$request['sessionKey']);
+    case "logout":
+    	return doLogout($request['username'],$request['sessionKey']);
   }
 }
 
-$server = new rabbitMQServer("loginRabbitMQ.ini","testServer");
+$server = new rabbitMQServer("testRabbitMQ.ini","testServer");
 $server->process_requests('requestProcessor');
 exit();
 ?>
