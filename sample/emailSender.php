586fd85731f07310b0b1a0e61647752873ef3f3f
Yousef Aboushaar <ya32@njit.edu>
2025-03-20 23:30:54 -0400

    installing php two factor auth library

--- sample/emailSender.php
+++ sample/emailSender.php
@@ -41,6 +41,37 @@ function sendRegistrationEmail($email)
 	} 
 }
 
+function sendTwoFactorAuthEmail($email)
+{
+	$mail = new PHPMailer(true);
+	//Server settings
+	$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+	$mail->isSMTP();                                            //Send using SMTP
+	$mail->Host       = 'smtp.gmail.com';                       //Set the SMTP server to send through
+	$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+	$mail->Username   = 'it490group@gmail.com';                 //SMTP username
+	$mail->Password   = 'it490password';                        //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        $mail->Port       = 465;       
+	       
+        $mail->setFrom('it490group@gmail.com', 'Mailer');     
+        $mail->addReplyTo('it490group@gmail.com', 'Information'); 
+        
+        $mail->isHTML(true);
+        $mail->Subject = 'Confirm Login';
+	$mail->Body = 'Hello $username, please confirm this is you.';
+	//$mail->AltBody = "";
+	
+	//Send the email
+	if (!$mail->send()) {
+	    echo "Message could not be sent to $email. Mailer Error: {$mail->ErrorInfo}\n" . PHP_EOL;
+	    return array('status' => 'error', 'message' => $mail->ErrorInfo);
+	} else {
+  	    echo "Message has been sent to $email\n" . PHP_EOL;
+  	    return array('returnCode' => '0', 'message' => $mail->ErrorInfo);
+	} 
+}
+
 function sendReadingGroupEmail($email, $groupName)
 {
 	$mail = new PHPMailer(true);
@@ -91,7 +122,10 @@ function requestProcessor($request)
   }
   case "sendgroupemail":
     	return sendReadingGroupEmail($request['username, groupname']);
-  }  	
+  }  
+  case "sendAuthenticaion":
+  	return sendTwoFactorAuthEmail($request['username']);	
+  }
 }
 
 $server = new rabbitMQServer("../rabbitMQ.ini","emailServer");
