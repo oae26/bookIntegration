e5ae11ab9ca24f8310f24e6e7da771161b41ba76
Franklin <fgw@njit.edu>
2025-04-09 10:14:57 -0400

    full file path for web

--- cluster_programs/zipBundle.php
+++ cluster_programs/zipBundle.php
@@ -17,8 +17,11 @@ else if($argv[1] == 'web'){ //run on web/backend machine
 	if($res === true){
 		echo "Making zip..." . PHP_EOL;
 		$options = array('add_path' => '/', 'remove_all_path' => TRUE);
-		//$options2 = array('add_path' => 'sql_programs/', 'remove_all_path' => TRUE);
+		$options2 = array('add_path' => 'node_modules/', 'remove_all_path' => TRUE);
+		$options3 = array('add_path' => 'vendor/', 'remove_all_path' => TRUE);
 		$zip->addGlob('/var/www/sample/*.*', GLOB_BRACE, $options);
+		$zip->addGlob('/var/www/sample/node_modules/*.*', GLOB_BRACE, $options2);
+		$zip->addGlob('/var/www/sample/vendor/*.*', GLOB_BRACE, $options3);
 		$zip->close();	
 		
 	}
