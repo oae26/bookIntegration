3c154d9c453676f7ad361f30209efdd2818eae8d
Yousef Aboushaar <ya32@njit.edu>
2025-04-20 18:23:06 -0400

    sql query updates, deploy case updates

--- clusterListeners/clusterListener.php
+++ clusterListeners/clusterListener.php
@@ -43,7 +43,15 @@ function logDeployment($request) {
 	$zipfile = $mydb->real_escape_string($request['zipFile']);
 	$description = $mydb->real_escape_string($request['description'] ?? '');
 	
-	$query = "INSERT INTO application_versions (version, source, destination, zip_file, description) VALUES ('$version', '$source', '$destination', '$zipfile', '$description')";
+	$query = "SELECT * from application_versions where source= '" . $source . "' and destination= '" . $destination . "' and version= '" . $version . "' and zip_file= '" . $zipfile . "';";
+	if ($response = $mydb->query($query)) {
+		if ($response->num_rows == 0) {
+			$query = "INSERT INTO application_versions (version, source, destination, zip_file, description) VALUES ('$version', '$source', '$destination', '$zipfile', '$description')";
+		}
+	} else {
+		$row = $reponse->fetch_row();
+		$query = "update application_versions set source= '" . $source . "' and destination= '" . $destination . "' and version= '" . $version . "' and zip_file= '" . $zipfile . "' where id= " . $row[0] . ";";
+	}
 		  
 	if (!$mydb->query($query)) {
 		echo "Error logging deployment: " . $mydb->error . PHP_EOL;
@@ -77,33 +85,78 @@ function requestProcessor($request) {
 	$targetPath = "~/releases/{$zipfile}";
 	
 	switch ($type) {
-		case 'deploy':
-			$sourceHost = parse_ini_file('clusterIPs.ini')[$source];
-			$destHost = parse_ini_file('clusterIPs.ini')[$destination];
-			
-			scpFile($user, $sourceHost, $zipRemotePath, $zipLocalPath);
-			logDeployment($request);
-			scpToCluster($zipLocalPath, $user, $destHost, $targetPath);
-			break;
-			
 		case 'web':
-		case 'sql':
-		case 'dmz':
 			if (!isset($request['destination'])) {
 				return "ERROR: Missing destination (qa or prod)";
 			}
 			
+			$zipRemotePath = "~/staging/{$zipfile}";
+			$zipLocalPath = "/home/yousef/deploy/staging/web/{$version}/{$zipfile}";
+			$targetPath = "~/var/www/sample/{$zipfile}";
+			
 			$role = $type;
 			$destination = $request['destination'];
-			$hostKey = "{$role}-{$destination}";
+			$hostKey = "{$role}-{$source}";
+			$targetKey = "{$role}-{$destination}";
 			
-			$targetHost = parse_ini_file('clusterIPs.ini')[$hostKey];
+			$sourceHost = parse_ini_file('clusterIPs.ini')[$hostKey];
+			$targetHost = parse_ini_file('clusterIPs.ini')[$targetKey];
 			
 			if (!$targetHost) {
 				return "ERROR: Target host for '$hostKey' not found in clusterIPs.ini";
 			}
 			
-			scpFile($user, $targetHost, $zipRemotePath, $zipLocalPath);
+			scpFile($user, $sourceHost, $zipRemotePath, $zipLocalPath);
+			logDeployment($request);
+			scpToCluster($zipLocalPath, $user, $targetHost, $targetPath);
+			echo "Sent zip to $type node ($targetHost)" . PHP_EOL;
+			break;
+		case 'sql':
+			if (!isset($request['destination'])) {
+				return "ERROR: Missing destination (qa or prod)";
+			}
+			$zipRemotePath = "~/staging/{$zipfile}";
+			$zipLocalPath = "/home/yousef/deploy/staging/sql/{$version}/{$zipfile}";
+			$targetPath = "~/srv/{$zipfile}";
+			
+			$role = $type;
+			$destination = $request['destination'];
+			$hostKey = "{$role}-{$source}";
+			$targetKey = "{$role}-{$destination}";
+			
+			$sourceHost = parse_ini_file('clusterIPs.ini')[$hostKey];
+			$targetHost = parse_ini_file('clusterIPs.ini')[$targetKey];
+			
+			if (!$targetHost) {
+				return "ERROR: Target host for '$hostKey' not found in clusterIPs.ini";
+			}
+			
+			scpFile($user, $sourceHost, $zipRemotePath, $zipLocalPath);
+			logDeployment($request);
+			scpToCluster($zipLocalPath, $user, $targetHost, $targetPath);
+			echo "Sent zip to $type node ($targetHost)" . PHP_EOL;
+			break;
+		case 'dmz':
+			if (!isset($request['destination'])) {
+				return "ERROR: Missing destination (qa or prod)";
+			}
+			$zipRemotePath = "~/staging/{$zipfile}";
+			$zipLocalPath = "/home/yousef/deploy/staging/dmz/{$version}/{$zipfile}";
+			$targetPath = "~/srv/{$zipfile}";
+			
+			$role = $type;
+			$destination = $request['destination'];
+			$hostKey = "{$role}-{$source}";
+			$targetKey = "{$role}-{$destination}";
+			
+			$sourceHost = parse_ini_file('clusterIPs.ini')[$hostKey];
+			$targetHost = parse_ini_file('clusterIPs.ini')[$targetKey];
+			
+			if (!$targetHost) {
+				return "ERROR: Target host for '$hostKey' not found in clusterIPs.ini";
+			}
+			
+			scpFile($user, $sourceHost, $zipRemotePath, $zipLocalPath);
 			logDeployment($request);
 			scpToCluster($zipLocalPath, $user, $targetHost, $targetPath);
 			echo "Sent zip to $type node ($targetHost)" . PHP_EOL;
@@ -118,7 +171,7 @@ function requestProcessor($request) {
 
 }
 
-$server = new rabbitMQServer("/home/yousef/git/bookIntegration/clusterListeners/deploymentRabbitMQ.ini","deployment");
+$server = new rabbitMQServer("/home/yousef/git/bookIntegration/clusterListeners/deploymentRabbitMQ.ini","mysqlDeployment");
 $server->process_requests('requestProcessor');
 exit();
 ?>
