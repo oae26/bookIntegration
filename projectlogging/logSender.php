e184c7d02f25fb5706759d8f82ebf0622b27cdd1
Franklin <fgw@njit.edu>
2025-04-27 21:05:23 -0400

    logs send timestamp and machine info

--- projectlogging/logSender.php
+++ projectlogging/logSender.php
@@ -4,17 +4,15 @@ require_once('../path.inc');
 require_once('../get_host_info.inc');
 require_once('../rabbitMQLib.inc');
 
-$client = new rabbitMQClient("../rabbitMQ.ini",'distLogging');
-
 function sendLog($message){
-	global $client;
+	$client = new rabbitMQClient("/rabbitmqini/rabbitMQ.ini",'distLogging');;
+	$timestamp = date('c');
+	$machine = gethostname();
+	$log = "[$timestamp][$machine] $message";
 	
 	$request = array();
 	$request['type'] = "log";
-	$request['message'] = time() . "this is a message" . PHP_EOL;
+	$request['message'] = $log;
 	$client->logPublish($request);
 }
-
-$message = time() . "this is a test message" . PHP_EOL;
-sendLog($message);
 ?>
