28bd9eab493f37e604f72094518f11d5d59ae8e0
Franklin <fgw@njit.edu>
2025-04-29 14:14:25 -0400

    beginnings of failover

--- projectlogging/logSender.php
+++ projectlogging/logSender.php
@@ -1,15 +1,15 @@
 #!/usr/bin/php
 <?php
-require_once('../path.inc');
-require_once('../get_host_info.inc');
-require_once('../rabbitMQLib.inc');
+require_once('/srv/path.inc');
+//require_once('/srv/get_host_info.inc');
+require_once('/srv/rabbitMQLib.inc');
 
 function sendLog($message){
-	$client = new rabbitMQClient("/rabbitmqini/rabbitMQ.ini",'distLogging');;
+	$client = new rabbitMQClient("/rabbitmqini/rabbitMQ.ini",'distLogging');
 	$timestamp = date('c');
 	$machine = gethostname();
 	$log = "[$timestamp][$machine] $message";
-	
+	echo "Logging...";
 	$request = array();
 	$request['type'] = "log";
 	$request['message'] = $log;
---
+++ sqlFailCheck.php
@@ -0,0 +1,44 @@
+#!/usr/bin/php
+<?php
+require('projectlogging/logSender.php');
+$error = false;
+
+$output1 = shell_exec("systemctl is-active projectlogin.service");
+$output2 = shell_exec("systemctl is-active projectbooksearch.service");
+$output3 = shell_exec("systemctl is-active projectrating.service");
+$output4 = shell_exec("systemctl is-active projectreview.service");
+$output5 = shell_exec("systemctl is-active projectgroups.service");
+$output6 = shell_exec("systemctl is-active projectwatchlist.service");
+
+if(trim($output1) != "active"){
+	sendLog("Login Service Error");
+	$error = true;
+}
+if(trim($output2) != "active"){
+	sendLog("Book Service Error");
+	$error = true;
+}
+if(trim($output3) != "active"){
+	sendLog("Rating Service Error");
+	$error = true;
+}
+if(trim($output4) != "active"){
+	sendLog("Review Service Error");
+	$error = true;
+}
+if(trim($output5) != "active"){
+	sendLog("Group Service Error");
+	$error = true;
+}
+if(trim($output6) != "active"){
+	sendLog("Watchlist Service Error");
+	$error = true;
+}
+
+if($error){
+	$client = new rabbitMQClient("/rabbitmqini/failover.ini",'sqlFail');
+	$request['type'] = "fail";
+	
+}
+
+?>
